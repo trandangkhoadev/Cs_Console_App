6200b2a86854dcad92605424105ca70184145b25
trand <trand@DESKTOP-FT17OFT>
2023-02-06 23:35:40 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cs_Console_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
